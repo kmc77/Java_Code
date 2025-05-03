81801924c47a9883548561e005fcfb8fe1a238bd
kmc77 <k_minchoi@naver.com>
2025-05-03 16:08:43 +0900

    기존 풀이 삭제

--- src/main/java/org/example/b1/P11720_숫자의합.java
+++ src/main/java/org/example/b1/P11720_숫자의합.java
@@ -6,17 +6,7 @@ import java.util.Scanner;
 public class P11720_숫자의합 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
 
-        String sNum = sc.next();
-        char[] cNum = sNum.toCharArray();
-        int sum = 0;
-
-        for (int i = 0; i < cNum.length; i++) {
-            sum += cNum[i] - '0'; // 문자(char) 형태의 숫자를 실제 정수(int)로 바꾸는 과정 자바에서 숫자는 아스키 코드 임
-
-        }
-        System.out.println("sum = " + sum);
     }
 
 }
--- src/main/java/org/example/b1/P1920_원하는정수찾기.java
+++ src/main/java/org/example/b1/P1920_원하는정수찾기.java
@@ -8,41 +8,6 @@ import java.util.StringTokenizer;
 
 public class P1920_원하는정수찾기 {
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.parseInt(br.readLine());
-        int[] A = new int[N];
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        for (int i = 0; i < N; i++) {
-            A[i] = Integer.parseInt(st.nextToken());
-        }
-        Arrays.sort(A);
-        int M = Integer.parseInt(br.readLine());
-        st = new StringTokenizer(br.readLine());
-        for (int i = 0; i < M; i++) {
-            boolean find = false;
-            int target = Integer.parseInt(st.nextToken());
-            //이진 탐색 시작
-            int start = 0;
-            int end = A.length - 1;
-            while (start <= end) {
-                int midi = (start + end) / 2;
-                int midV = A[midi];
-                if (midV > target) {
-                    end = midi - 1;
-                } else if (midV < target) {
-                    start = midi + 1;
-                } else {
-                    find = true;
-                    break;
-                }
-            }
-            if (find) {
-                System.out.println(1);
-            }
-            else
-            {
-                System.out.println(0);
-            }
-        }
+
     }
 }
