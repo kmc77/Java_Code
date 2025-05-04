b362ba5cae00bf9fb5cb6a5f7a2943b9ceaefeab
kmc77 <k_minchoi@naver.com>
2025-05-04 18:06:30 +0900

    배열 합치기 및 정렬(주석 추가)

--- src/main/java/org/example/test_4/배열합치기및정렬.java
+++ src/main/java/org/example/test_4/배열합치기및정렬.java
@@ -10,7 +10,7 @@ public class 배열합치기및정렬 {
 
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        Set<Integer> setA = new TreeSet<>();
+        Set<Integer> setA = new TreeSet<>(); // TreeSet : 중복 제거, 자동 정렬, 빠른 탐색
         for (int i = 0; i < n; i++) {
             setA.add(sc.nextInt());
         }
@@ -22,6 +22,7 @@ public class 배열합치기및정렬 {
 
         int x = sc.nextInt();
 
+        //hasNext()로 데이터가 남았는지 확인하고, next()로 데이터를 순차적으로 가져옴.
         Iterator<Integer> it = setA.iterator();
         while (it.hasNext()) {
             int num = it.next();
