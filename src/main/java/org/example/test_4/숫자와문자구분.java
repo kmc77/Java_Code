5eeb11940fb2518e61a2ebc67a1a6b4d64dea1ee
kmc77 <k_minchoi@naver.com>
2025-05-06 13:37:32 +0900

    feat: 입력 문자열에서 숫자만 추출하여 출력하는 기능 구현
    - Scanner를 사용하여 사용자 입력 처리
    
    - Character.isDigit()으로 숫자 판별
    
    - StringBuilder를 활용하여 문자열 결합 시 성능 최적화

--- src/main/java/org/example/test_4/숫자와문자구분.java
+++ src/main/java/org/example/test_4/숫자와문자구분.java
@@ -6,8 +6,8 @@ public class 숫자와문자구분 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String input = sc.nextLine();
-        // String 대신 StringBuilder를 사용하여
-        // 문자열을 동적으로 연결함으로써 메모리 소모를 줄이고 속도를 향상
+
+        // String 대신 StringBuilder를 사용하여 속도를 향상
         StringBuilder sb = new StringBuilder();
         for (int i = 0; i < input.length(); i++) {
             char c = input.charAt(i);
