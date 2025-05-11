5a01674243e41f039cb87f3637843c7cc5227432
kmc77 <k_minchoi@naver.com>
2025-05-11 13:06:01 +0900

    feat: P10172_개

---
+++ src/main/java/org/example/b1/P10172_개.java
@@ -0,0 +1,14 @@
+package org.example.b1;
+
+import java.util.Scanner;
+
+public class P10172_개 {
+    public static void main(String[] args) {
+        System.out.println("|\\_/|");
+        System.out.println("|q p|   /}");
+        System.out.println("( 0 )\"\"\"\\");
+        System.out.println("|\"^\"`    |");
+        System.out.println("||_/=\\\\__|");
+
+    }
+}
