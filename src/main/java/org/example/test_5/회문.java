672f1bd1bccbd3a1a670dbcdeea97007b0f7a824
kmc77 <k_minchoi@naver.com>
2025-05-15 22:02:21 +0900

    solve: 최소 삽입으로 회문 만들기 문제
    주어진 문자열을 회문으로 만들기 위해 필요한 최소 삽입 횟수를 계산하는 DP 기반 알고리즘2차원 DP 테이블을 활용하여 부분 문자열 간 최소 삽입 횟수를 누적 계산.- 점화식: s[i] == s[j]일 경우 dp[i][j] = dp[i+1][j-1],  다를 경우 dp[i][j] = min(dp[i+1][j], dp[i][j-1]) + 1- 시간복잡도: O(n^2), 공간복잡도: O(n^2)- 예시: 'abca' -> 최소 1회 삽입으로 회문 가능
    
    관련 문제: LeetCode 1312 / 백준 유사 문제 참고

---
+++ src/main/java/org/example/test_5/회문.java
@@ -0,0 +1,25 @@
+package org.example.test_5;
+
+public class 회문 {
+    public static int main(String s) {
+
+        int n = s.length();
+        int[][] dp = new int[n][n];
+
+        for (int len = 2; len <= n; len++) {
+            for (int i = 0; i < n - len + 1; i++) {
+                int j = i + len - 1;
+                if (s.charAt(i) == s.charAt(j)) {
+                    dp[i][j] = dp[i + 1][j - 1];
+                } else {
+                    dp[i][j] = Math.min(dp[i + 1][j], dp[i][j - 1]) + 1;
+                }
+            }
+        }
+        return dp[0][n - 1];
+    }
+
+    public static void main(String[] args) {
+        System.out.println(main("abca")); // 1
+    }
+}
