42848a82392a912241e2ac10ecea625f20ae667b
kmc77 <k_minchoi@naver.com>
2025-05-15 21:58:07 +0900

    solve: HashMap을 이용한 Two Sum 문제
    Two Sum 문제에서 배열 내 두 수의 합이 target이 되는 인덱스 쌍을 찾는 알고리즘HashMap을 활용해 현재 숫자의 보완 수(complement)를 key로 저장하면서 한 번의 순회
    - 입력: 정수 배열과 target- 출력: target을 만족하는 두 수의 인덱스- 시간 복잡도: O(n)
    
    Resolves: #two-sum

---
+++ src/main/java/org/example/test_5/HashMap을이용한TwoSum.java
@@ -0,0 +1,36 @@
+package org.example.test_5;
+
+import java.util.HashMap;
+import java.util.Scanner;
+
+public class HashMap을이용한TwoSum {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int[] nums = new int[n];
+        for (int i = 0; i < n; i++) {
+            nums[i] = sc.nextInt();
+        }
+        int target = sc.nextInt();
+
+        int[] result = twoSum(nums, target);
+
+        System.out.println("[" + result[0] + "," + result[1] + "]");
+
+
+    }
+
+    public static int[] twoSum(int[] nums, int target) {
+        HashMap<Integer, Integer> hM = new HashMap<>();
+
+        for (int i = 0; i < nums.length; i++) {
+            int complement = target - nums[i];
+            if (hM.containsKey(complement)) {
+                return new int[]{hM.get(complement), i};
+            }
+            hM.put(nums[i], i);
+        }
+        return new int[]{};
+    }
+}
