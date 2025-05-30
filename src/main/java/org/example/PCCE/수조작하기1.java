dd659de7d210f3bc50c3bd0707711ea97d997d49
kmc77 <k_minchoi@naver.com>
2025-05-30 21:33:38 +0900

    solve: 제어 문자열에 따라 수 조작
    문자열 control을 순회하며 각 문자에 따라 정수 n을 증감:
    - 'w'는 +1
    - 's'는 -1
    - 'd'는 +10
    - 'a'는 -10
    간단한 명령어 기반 수 조작 로직

---
+++ src/main/java/org/example/PCCE/수조작하기1.java
@@ -0,0 +1,21 @@
+package org.example.PCCE;
+
+public class 수조작하기1 {
+    public int solution(int n, String control) {
+
+        for (int i = 0; i < control.length(); i++) {
+            char c = control.charAt(i);
+
+            if (c == 'w') {
+                n += 1;
+            } else if (c == 's') {
+                n -= 1;
+            } else if (c == 'd') {
+                n += 10;
+            } else if (c == 'a') {
+                n -= 10;
+            }
+        }
+        return n;
+    }
+}
