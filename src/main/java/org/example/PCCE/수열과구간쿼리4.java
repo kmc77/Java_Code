5d1a09aa0e07f7ffc45b1fb5db8c1fea10bd261d
kmc77 <k_minchoi@naver.com>
2025-06-03 15:54:12 +0900

    solve: 특정 구간에서 k의 배수 인덱스 값 증가 로직
    쿼리 [s, e, k]에 대해 s 이상 e 미만 범위 내에서
    인덱스가 k의 배수인 위치의 값을 1씩 증가시킴.

---
+++ src/main/java/org/example/PCCE/수열과구간쿼리4.java
@@ -0,0 +1,22 @@
+package org.example.PCCE;
+
+public class 수열과구간쿼리4 {
+    public int[] solution(int[] arr, int[][] queries) {
+
+        for (int i = 0; i < queries.length; i++) {
+            int s = queries[i][0];
+            int e = queries[i][1];
+            int k = queries[i][2];
+
+            for (int j = s; j < e; j++) {
+                if (j % k == 0) {
+                    arr[j] += 1;
+                }
+            }
+        }
+        return arr;
+    }
+}
+
+
+
