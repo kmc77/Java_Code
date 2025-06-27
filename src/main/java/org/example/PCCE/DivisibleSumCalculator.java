cd60ede25d16a98941c7134b2b9f72aeb937ecc1
kmc77 <k_minchoi@naver.com>
2025-06-27 23:26:21 +0900

    solve: 3, 5, 7로 나누어 떨어지는 수의 합 구하는 기능
    - 입력된 n까지의 수 중에서 3, 5, 7로 나누어 떨어지는 수 판별
    - 조건을 만족하는 수들을 누적하여 총합 계산
    - 예외 조건 없이 단순 반복문으로 처리하여 안정적 동작 확보

---
+++ src/main/java/org/example/PCCE/DivisibleSumCalculator.java
@@ -0,0 +1,15 @@
+package org.example.PCCE;
+
+public class DivisibleSumCalculator {
+    public int solution(int n) {
+        int sum = 0;
+
+        for (int i = 1; i <= n; i++) {
+            if (i % 3 == 0 || i % 5 == 0 || i % 7 == 0) {
+                sum += i;
+            }
+        }
+
+        return sum;
+    }
+}
